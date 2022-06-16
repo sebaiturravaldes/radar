b980e1c9f112d7806b93c5bce5fab16527a3c751
Sebastián Iturra <iturra.sebastian@hotmail.com>
2022-06-16 14:35:34 -0400

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# radar
-Created with CodeSandbox
+# Radar
+
+El Radar, es creado con la finalidad de poder registrar algo que debes realizar en un tiempo determimado, pero que no es una preocupación actual.
+
+Puedes utilizar recordatorios, pero que mejor que tener tu propio radar, al alcance de tu mano.
+
+## Demo
+
+[https://5jlfq2.csb.app/](https://5jlfq2.csb.app/)
+
+## TODO
+
+- [ ] Mientras más cerca te encuentras de la fecha de caducidad más cerca debiesen estar del punto central.
+- [ ] Registrar nuevos datos en el radar.
+- [ ] Modificar los datos que registras.
+- [ ] Evaluar si lo pasamos a una App instalable o lo lanzamos a un dominio en un sitio web.
+- [ ] Agregar el sonido del radar.
+
+## Inspired by
+
+This code is inspired by [CSS Dragon Ball Radar](https://codepen.io/jsmask/pen/WNwXOmN)
---
+++ src/App.js
@@ -0,0 +1,45 @@
+import "./styles.css";
+import { useState } from "react";
+const items = [
+  {
+    title: "Multa",
+    date: "25-06-2022"
+  },
+  {
+    title: "Afiliación Catalina",
+    date: "30-06-2022"
+  },
+  {
+    title: "Reembolso Isapre",
+    date: "30-06-2022"
+  }
+];
+
+export default function App() {
+  const [isChecked, setIsChecked] = useState(true);
+
+  const handleOnChange = () => {
+    setIsChecked(!isChecked);
+  };
+
+  return (
+    <div class="main">
+      <div class="randar-box">
+        <input
+          type="checkbox"
+          class="randar-checkbox"
+          checked={isChecked}
+          onChange={handleOnChange}
+        />
+        <div class="randar-top"></div>
+        <div class="randar-top-body"></div>
+        <div class="randar-content">
+          {items.map((item) => (
+            <div class="rander-ball">{item.title}</div>
+          ))}
+          <div class="rander-pos"></div>
+        </div>
+      </div>
+    </div>
+  );
+}
